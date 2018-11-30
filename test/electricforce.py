40325d08aedb2be0618748fa7a8a9a48fcfc2656
taisei07 <tennis.taisei1007@gmail.com>
2018-12-01 02:12:02 +0900

    update

--- test/electricforce.py
+++ test/electricforce.py
@@ -367,12 +367,15 @@ while t <= T:
                 i += 1
             i = 1
             j += 1
-        #boundary_condition()
+        boundary_condition()
         phi_old = phi
         #Gaussの法則
         phi_calculation()
         B = phi - phi_old
         Bmax = np.max(B)
+        with open(os.path.join(str(value[1]),"B.csv"), 'w') as file:
+            writer = csv.writer(file, lineterminator = '\n')
+            writer.writerows(B)
         print "Bmax = " + str(Bmax)
         m1 += 1
     csvout01()
