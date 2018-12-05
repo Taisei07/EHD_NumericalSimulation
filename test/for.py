51e66260332fb76bde1cb91819342c56514e02d3
taisei07 <tennis.taisei1007@gmail.com>
2018-12-05 16:33:54 +0900

    update

--- test/for.py
+++ test/for.py
@@ -11,7 +11,7 @@ print "End"
 process_time = 10
 
 requests.post('https://hooks.slack.com/services/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', data = json.dumps({
-    'text': str(value[1]) + '1', # 投稿するテキスト #+ \n + 'process_time : ' + str(process_time)
+    'text': str(value[1]) + '\n' + 'process_time : ' + str(process_time), # 投稿するテキスト
     'username': u'ghost', # 投稿のユーザー名
     'icon_emoji': u':ghost:', # 投稿のプロフィール画像に入れる絵文字
     'link_names': 1, # メンションを有効にする
