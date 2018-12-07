225ec1986290b1c550c1536a52a3699feb2935b8
taisei07 <tennis.taisei1007@gmail.com>
2018-12-07 12:51:19 +0900

    update

--- test/for.py
+++ test/for.py
@@ -17,11 +17,13 @@ requests.post('https://hooks.slack.com/services/T9VCMG1QR/BEK19U49W/FEbd9qAfZCK0
     'link_names': 1, # メンションを有効にする
 }))
 TOKEN = 'xoxb-335429545841-497705575700-mYE1iTJjnFu05W2Cs1nLISav'
+CHANNEL = 'CELQHE10X'
 files = {'file': open("figure.png", 'rb')}
 param = {
 'token':TOKEN,
+'channels':CHANNEL,
 'filename':"figure.png",
 'initial_comment': "initial_comment",
 'title': "title"
 }
-requests.post(url="https://hooks.slack.com/services/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",params=param, files=files)
+requests.post(url="https://slack.com/api/files.upload",params=param, files=files)
