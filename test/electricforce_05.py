c5126f63a6a21a3ea06f1c33926066f683c4c463
taisei07 <tennis.taisei1007@gmail.com>
2018-12-15 16:59:28 +0900

    update

--- test/electricforce_05.py
+++ test/electricforce_05.py
@@ -23,7 +23,7 @@ import requests
 def slack_mention():
     process_time = time.time() - start_time
     requests.post('https://hooks.slack.com/services/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', data = json.dumps({
-        'text': str(value[1]) + '\n' + 'process_time : ' + str(process_time), # 投稿するテキスト
+        'text': str(value[1]) + '\n' + 'process_time : ' + str(process_time)　+ '\n' + 't : ' + str(t), # 投稿するテキスト
         'username': u'ghost', # 投稿のユーザー名
         'icon_emoji': u':ghost:', # 投稿のプロフィール画像に入れる絵文字
         'link_names': 1, # メンションを有効にする
