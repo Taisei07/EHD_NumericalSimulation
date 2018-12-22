00dc93a7eaf002957d8bf65ecdc11fc3aa6a7de8
Taisei07 <https://github.com/Taisei07/test.git>
2018-12-22 16:51:11 +0900

    update

--- test/gpu.py
+++ test/gpu.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import cupy as cp
-A = cp.arange(9).reshape(3, 3).astype('f') #cupy上で3*3の行列を生成
-B = cp.arange(9).reshape(3, 3).astype('f') #cupy上で3*3の行列を生成
-print('A = \n', A)
-print('B = \n', B)
+
+x_cpu = np.zeros((10,10))
+
+x_gpu = cp.asarray(x_cpu)
+x_cpu = cp.asnumpy(x_gpu)
+
+print(x_gpu ** 2)
+
+xp = cp.get_array_module(x_gpu)
+print(xp.square(x_gpu))
