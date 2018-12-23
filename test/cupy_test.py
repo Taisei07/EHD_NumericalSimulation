f09d9a426140fd1240a98b5d2a4740d9bd779651
taisei07 <tennis.taisei1007@gmail.com>
2018-12-23 23:23:38 +0900

    update

--- test/cupy_test.py
+++ test/cupy_test.py
@@ -5,4 +5,9 @@ import numpy
 a = numpy.random.rand(100)
 b = numpy.random.rand(100)
 
-numpy.linalg.norm(a - b)
+print(numpy.linalg.norm(a - b))
+
+a = numpy.random.rand(100)
+B = numpy.random.rand(1000000, 100)
+
+print([numpy.linalg.norm(a - b) for b in B])
